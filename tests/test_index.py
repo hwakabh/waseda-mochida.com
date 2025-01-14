18f1aadb296ba981d2666b2f6266a1a4619f8141
hwakabh <hrykwkbys1024@gmail.com>
2025-01-14 14:03:36 +0900

    refactor: moved frontend codes into Python packages: apps.

--- tests/test_index.py
+++ tests/test_index.py
@@ -3,6 +3,11 @@ import json
 from apps.index import app
 
 
+def tests_index_status_code_ok():
+     client = app.test_client()
+     assert client.get('/').status_code == 200
+
+
 def tests_health_status_ok():
     client = app.test_client()
     assert client.get('/healthz').status_code == 200
